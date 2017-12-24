baddf17e1f15bd6abf175d40d3e1dbf6f9fd06a7
fushuai-fs <fushuai_f@126.com>
2017-12-24 11:50:55 +0800

    WebView加载网易新闻Demo

---
+++ WebViewDemo.js
@@ -0,0 +1,63 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+
+
+import React, { Component } from 'react';
+import {
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    NativeModules,
+    NetInfo,
+    ToastAndroid,
+    Dimensions,
+    WebView
+} from 'react-native';
+
+//获取设备的宽度和高度
+var {
+    height: deviceHeight,
+    width: deviceWidth
+} = Dimensions.get('window');
+
+export default class WebViewDemo extends  Component<{}>  {
+    constructor(props){
+        super(props);
+        this.state = {
+            isConnected: null,
+            connectionInfo:null,
+        };
+    }
+    componentDidMount() {
+
+
+    }
+    componentWillUnmount() {
+    }
+    render() {
+        return (
+            <View style={{flex:1}}>
+                <WebView bounces={false}
+                         scalesPageToFit={true}
+                         startInLoadingState={true}
+                         domStorageEnabled={true}
+                         javaScriptEnabled={true}
+                         source={{uri:'https://news.163.com/',method: 'GET'}}
+                         style={{width:deviceWidth, height:deviceHeight}}
+
+                >
+                </WebView>
+            </View>
+        );
+    }
+}
+const styles = StyleSheet.create({
+    welcome: {
+        fontSize: 16,
+        textAlign: 'left',
+        margin: 10,
+    },
+});
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import NetWorkStatus from './NetWorkStatus'
+import  WebViewDemo from './WebViewDemo'
 
-AppRegistry.registerComponent('ReactNativeUpgradeDemo', () => NetWorkStatus);
+AppRegistry.registerComponent('ReactNativeUpgradeDemo', () => WebViewDemo);
