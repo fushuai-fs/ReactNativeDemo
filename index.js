48b06fb6d76dd6d6db213cc93b53e423ce0e7403
fushuai-fs <fushuai_f@126.com>
2018-01-07 20:38:59 +0800

    Demo

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
-import { AppRegistry } from 'react-native';
+
+import React from "react";
+import { AppRegistry ,BackHandler } from 'react-native';
+import { addNavigationHelpers, NavigationActions,TabNavigator } from "react-navigation";
+
 import App from './App';
 import NetWorkStatus from './NetWorkStatus'
 import  WebViewDemo from './WebViewDemo'
 
-AppRegistry.registerComponent('ReactNativeUpgradeDemo', () => WebViewDemo);
+const AppNavigation = TabNavigator(
+    {
+        Home: { screen: App },
+        NetWorkStatus: { screen: NetWorkStatus },
+        WebViewDemo: { screen: WebViewDemo }
+    }
+);
+ export default class ReduxNavigation extends Component<{}> {
+    componentDidMount() {
+        BackHandler.addEventListener("hardwareBackPress", this.onBackPress);
+    }
+    componentWillUnmount() {
+        BackHandler.removeEventListener("hardwareBackPress", this.onBackPress);
+    }
+    onBackPress = () => {
+        const { dispatch, nav } = this.props;
+        if (nav.index === 0) {
+            return false;
+        }
+        dispatch(NavigationActions.back());
+        return true;
+    };
+
+    render() {
+        const { dispatch, nav } = this.props;
+        const navigation = addNavigationHelpers({
+            dispatch,
+            state: nav
+        });
+
+        return <AppNavigation navigation={navigation} />;
+    }
+}
+
+AppRegistry.registerComponent('ReactNativeUpgradeDemo', () => ReduxNavigation);
