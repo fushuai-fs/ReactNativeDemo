fa977ea78cad18420fa7307b6a701a61892d133d
fushuai <fushuai_f@126.com>
2018-01-24 09:13:33 +0800

    gits

---
+++ android/app/src/main/java/com/reactnativeupgradedemo/MyService.java
@@ -0,0 +1,218 @@
+package com.reactnativeupgradedemo;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Binder;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.lang.Thread;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import RNDevice.RNDeviceModule;
+import okhttp3.FormBody;
+import okhttp3.Request;
+
+public class MyService extends Service {
+    public MyService() {
+        // 第二个参数为首次执行的延时时间，第三个参数为定时执行的间隔时间
+//        service.scheduleAtFixedRate(runnable, 10, 10, TimeUnit.SECONDS);
+
+    }
+
+//    ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+//Runnable runnable = new Runnable() {
+    //        @Override
+//        public void run() {
+//            i++;
+//        }
+//    };
+    public  static String UploadUri="";
+    public  static  int i=0;
+    private IBinder binder=new MyService.LocalBinder();
+
+    //定位都要通过LocationManager这个类实现
+    private LocationManager locationManager;
+    private String provider;
+    public  static String str="";
+    public  static String httpresult="";
+    public static RNDeviceModule device;
+//   private Thread thread = new Thread(new Runnable() {
+//        @Override
+//        public void run() {
+////          str +="locationService";
+////            Toast.makeText(getApplicationContext(),"locationService", Toast.LENGTH_LONG).show();
+//
+//
+//        }
+//    });
+
+   private void locationService(){
+       try {
+           //  获取的是位置服务
+           String serviceString = Context.LOCATION_SERVICE;
+           //  调用getSystemService()方法来获取LocationManager对象
+           locationManager = (LocationManager) getSystemService(serviceString);
+           //获取当前可用的位置控制器
+           List<String> list = locationManager.getProviders(true);
+           if (list.contains(LocationManager.GPS_PROVIDER)) {
+               //是否为GPS位置控制器
+               provider = LocationManager.GPS_PROVIDER;
+           }
+           else if (list.contains(LocationManager.NETWORK_PROVIDER)) {
+               //是否为网络位置控制器
+               provider = LocationManager.NETWORK_PROVIDER;
+
+           } else {
+//               Toast.makeText(getApplicationContext(), "请检查网络或GPS是否打开", Toast.LENGTH_LONG).show();
+               str +="请检查网络或GPS是否打开";
+               return;
+           }
+         Location location = locationManager.getLastKnownLocation(provider);
+           if (location != null) {
+               //获取当前位置，这里只用到了经纬度
+                 str += "纬度为：" + location.getLatitude() + ",经度为："
+                       + location.getLongitude();
+//               Toast.makeText(getApplicationContext(),str, Toast.LENGTH_LONG).show();
+               UploadLocation(String.valueOf(location.getLongitude()),String.valueOf(location.getLatitude()));
+           }
+
+           //绑定定位事件，监听位置是否改变
+//第一个参数为控制器类型
+// 第二个参数为监听位置变化的时间间隔（单位：毫秒）
+//第三个参数为位置变化的间隔（单位：米）第四个参数为位置监听器
+        locationManager.requestLocationUpdates(provider, 5000, 2,  locationListener);
+       }
+       catch (Exception ex)
+       {
+          str += ex.toString();
+           // Toast.makeText(getApplicationContext(),ex.toString(), Toast.LENGTH_LONG).show();
+       }
+    }
+
+    LocationListener locationListener = new LocationListener() {
+
+        @Override
+        public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onProviderEnabled(String enabled) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onProviderDisabled(String disabled) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onLocationChanged(Location location) {
+            // TODO Auto-generated method stub
+            // 更新当前经纬度
+            try {
+                if (location != null) {
+                    //获取当前位置，这里只用到了经纬度
+                    str += "纬度为：" + location.getLatitude() + ",经度为："
+                            + location.getLongitude();
+                    UploadLocation(String.valueOf(location.getLongitude()),String.valueOf(location.getLatitude()));
+                }
+            }catch (Exception ex){str+="update"+ex.toString();}
+        }
+    };
+
+    public void UploadLocation(String Longitude,String Latitude)
+    {
+        DateFormat df = DateFormat.getDateTimeInstance();
+        httpresult+=df.format(new Date())+"------";
+        FormBody formBody = new FormBody.Builder().add("DeviceID", "test")
+                .add("Longitude", Longitude)
+                .add("Latitude", Latitude)
+                .add("Mark", String.valueOf(i))
+                .add("date", df.format(new Date()))
+                .build();
+
+        OkHttpHelper.getInstance().asyncPost(UploadUri, formBody, new OkHttpHelper.HttpCallBack() {
+            @Override
+            public void onError(Request request, IOException e) {
+                httpresult += e.toString();
+            }
+
+            @Override
+            public void onSuccess(Request request, String result) {
+                httpresult+="Success";
+            }
+
+        });
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        return binder;
+    }
+
+    @Override
+    public void onCreate() {
+        str+="OnCreate";
+        locationService();
+        super.onCreate();
+    }
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+//        str+=thread.getState().toString();
+//        switch (thread.getState())
+//        {
+//            case NEW:
+//                thread.start();
+//                break;
+//            case TERMINATED:
+//                thread.start();
+//                break;
+//            case RUNNABLE:
+//                thread.notify();
+//                break;
+//            case BLOCKED:
+//                thread.notify();
+//                break;
+//            case WAITING :break;
+//            case TIMED_WAITING :break;
+//        }
+//        str+=thread.getState().toString();
+        return super.onStartCommand(intent, flags, startId);
+
+    }
+
+    @Override
+    public void onDestroy() {
+        if (locationManager != null) {
+        locationManager.removeUpdates(locationListener);
+    }
+        super.onDestroy();
+
+    }
+
+    //定义内容类继承Binder
+    public class LocalBinder extends Binder {
+        //返回本地服务
+        MyService getService(){
+            return MyService.this;
+        }
+    }
+}
---
+++ android/app/src/main/java/com/reactnativeupgradedemo/OkHttpHelper.java
@@ -0,0 +1,106 @@
+package com.reactnativeupgradedemo;
+
+
+import android.os.Handler;
+import android.os.Looper;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * Created by fus on 2018/01/19.
+ *
+ FormBody formBody = new FormBody.Builder().add("username", "lesiefang").add("age", "28").build();
+ MyOkHttpClient.getInstance().asyncPost("http://192.168.94.131:5000/aaa/", formBody, new MyOkHttpClient.HttpCallBack() {
+@Override
+public void onError(Request request, IOException e) {
+Log.d("aaa", e.toString());
+}
+
+@Override
+public void onSuccess(Request request, String result) {
+textView.setText(result);
+}
+});
+ */
+
+public class OkHttpHelper {
+
+    private static OkHttpHelper myOkHttpClient;
+    private OkHttpClient okHttpClient;
+    private Handler handler;
+
+    private OkHttpHelper() {
+        okHttpClient = new OkHttpClient();
+        handler = new Handler(Looper.getMainLooper());
+    }
+
+    public static OkHttpHelper getInstance() {
+        if (myOkHttpClient == null) {
+            synchronized (OkHttpHelper.class) {
+                if (myOkHttpClient == null) {
+                    myOkHttpClient = new OkHttpHelper();
+                }
+            }
+        }
+        return myOkHttpClient;
+    }
+    public void asyncGet(String url, HttpCallBack httpCallBack) {
+        Request request = new Request.Builder().url(url).build();
+        okHttpClient.newCall(request).enqueue(new StringCallBack(request, httpCallBack));
+    }
+
+    public void asyncPost(String url, FormBody formBody, HttpCallBack httpCallBack) {
+        Request request = new Request.Builder().url(url).post(formBody).build();
+        okHttpClient.newCall(request).enqueue(new StringCallBack(request, httpCallBack));
+    }
+
+
+
+    public interface HttpCallBack {
+        void onError(Request request, IOException e);
+
+        void onSuccess(Request request, String result);
+    }
+    class StringCallBack implements Callback {
+        private HttpCallBack httpCallBack;
+        private Request request;
+
+        public StringCallBack(Request request, HttpCallBack httpCallBack) {
+            this.request = request;
+            this.httpCallBack = httpCallBack;
+        }
+        @Override
+        public void onFailure(Call call, IOException e) {
+            final IOException fe = e;
+            if (httpCallBack != null) {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        httpCallBack.onError(request, fe);
+                    }
+                });
+            }
+        }
+
+        @Override
+        public void onResponse(Call call, Response response) throws IOException {
+            final String result = response.body().string();
+            if (httpCallBack != null) {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        httpCallBack.onSuccess(request, result);
+                    }
+                });
+            }
+        }
+    }
+}
+
